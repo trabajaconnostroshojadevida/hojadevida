bf27f9d167ebad1eede67200ecaa4ec0746f7900
ALEJANDRO USME GALLEGO <alejandrousme235808@correo.itm.edu.co>
2026-01-29 22:48:05 -0500

    Primera parte

--- src/app/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    supabase: {
-        url: 'https://hfoacgglqjdbyoafqwcq.supabase.co',
-        anonKey: 'sb_publishable_JBBup8z8kvt3DPnN28JEoA_XcYjBK1R'
-    }
-};
